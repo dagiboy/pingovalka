dad7e65a2f07f60f9401267e308e81a296cb9494
dagiboy <brain.light05@gmail.com>
2024-08-31 19:39:31 +0300

    added flapodav

--- monitoring.py
+++ monitoring.py
@@ -2,13 +2,19 @@ import asyncio
 import telegram
 import aiohttp
 import json
+from dataclasses import dataclass
 
 CONFIGS = json.load(open("config.json"))
 BOT = telegram.Bot(CONFIGS["bot_token"])
 SITES = CONFIGS["sites"]
 BAD_NEWS_CHAT_ID = CONFIGS["notification_chat_id"]
 
-STATUS = dict()
+@dataclass
+class TSiteStatus:
+    status: bool
+    counter: int
+
+STATUS: dict[str, TSiteStatus] = dict()
 
 async def is_response_ok(response) -> bool:
     if response.status != 200:
@@ -33,15 +39,21 @@ async def start_monitoring(site: str):
             error = "Can't request"
 
         try:
-            if is_ok:
-                if not STATUS[site]["status"]:
-                    message = "Site: " + site + "\n" + "is alive"
-                    await BOT.send_message(text = message, chat_id=BAD_NEWS_CHAT_ID)
-            else:
-                message = "Site: " + site + "\n" + error
-                await BOT.send_message(text = message, chat_id=BAD_NEWS_CHAT_ID)
+            if is_ok and not STATUS[site].status:
+                STATUS[site].counter = max(0, min(5, STATUS[site].counter - 1))
 
-            STATUS[site]["status"] = is_ok
+                if STATUS[site].counter == 0:
+                    STATUS[site].status = True
+                    message = "Site: " + site + "\n" + "is alive"
+                    await BOT.send_message(text = message, chat_id=BAD_NEWS_CHAT_ID)             
+            elif not is_ok:
+                STATUS[site].counter = min(STATUS[site].counter + 1, 100)
+
+                if 3 <= STATUS[site].counter <= 13:
+                    STATUS[site].status = False
+                    message = "Site: " + site + "\n" + error
+                    await BOT.send_message(text = message, chat_id=BAD_NEWS_CHAT_ID)
+
         except Exception as e:
             print(e)
 
@@ -50,11 +62,15 @@ async def start_monitoring(site: str):
 async def im_ok():
     await asyncio.sleep(60*60*9)
     while True:
-        await BOT.send_message(text = "I'm alive", chat_id=BAD_NEWS_CHAT_ID)
+        message = "Bot is alive"
+        for site, site_status in STATUS.items():
+            if not site_status.status:
+                message += '\nThe ' + site + " is dead"
+        await BOT.send_message(text = message, chat_id=BAD_NEWS_CHAT_ID)
         await asyncio.sleep(60*60*24)
 
 for site in SITES:
-    STATUS[site] = {"status": True}
+    STATUS[site] = TSiteStatus(True, 0)
 
 if __name__ == "__main__":
     ioloop = asyncio.get_event_loop()
